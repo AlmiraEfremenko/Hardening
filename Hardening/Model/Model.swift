d2de484f75c1aaaaec148d51b948aaff054b65ce
AlmiraEfremenko <almira.efremenko@yandex.ru>
2022-04-26 15:46:53 +0500

    Add model.

--- Hardening/Model/Model.swift
+++ Hardening/Model/Model.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+struct Model {
+    var titleLarge: String
+    var titleInCardLeft: String
+    var textAboutCostLeft: String
+    var textAboutRateLeft: String
+    var textCommentAboutSubscription: String
+    var textAboutSubscription: String
+}
+
+extension Model {
+    
+    static var data = Model(titleLarge: "Выбери подписку и попробуй 7 дней без оплаты",
+                            titleInCardLeft: "1 месяц",
+                            textAboutCostLeft: "379 руб. в месяц",
+                            textAboutRateLeft: "Ежемесячная оплата, первые 7 дней без оплаты",
+                            textCommentAboutSubscription: "Отписаться можно в любой момент",
+                            textAboutSubscription: "Восстановить подписку или Войти")
+}
