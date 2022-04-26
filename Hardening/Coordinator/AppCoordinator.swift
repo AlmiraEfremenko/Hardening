519b1cd64a99e26c61517b80bcf56f23499fcee6
AlmiraEfremenko <almira.efremenko@yandex.ru>
2022-04-26 15:46:44 +0500

    Add appCoordinator.

--- Hardening/Coordinator/AppCoordinator.swift
+++ Hardening/Coordinator/AppCoordinator.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import UIKit
+
+// AppCoordinator реализует протокол Coordinator
+
+class AppCoordinator: Coordinator {
+    
+    let window: UIWindow
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        let navigationController = UINavigationController()
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        window.backgroundColor = .white
+        let startCoordinator = MainScreenCoordinator(navigationController: navigationController)
+        startCoordinator.start()
+    }
+}
