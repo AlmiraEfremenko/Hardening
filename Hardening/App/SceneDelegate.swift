5d2b3b876ff448238d747f49c2dc305f139aff19
AlmiraEfremenko <almira.efremenko@yandex.ru>
2022-05-11 20:06:23 +0500

    Add changes scene.

--- Hardening/App/SceneDelegate.swift
+++ Hardening/App/SceneDelegate.swift
@@ -15,6 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
+        //Thread.sleep(forTimeInterval: 5)
         coordinator = AppCoordinator(window: window!)
         coordinator?.start()
     }
