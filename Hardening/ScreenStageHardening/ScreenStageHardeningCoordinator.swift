ecf8180bf0cbc88c9c7332422f950c0c7e13e908
AlmiraEfremenko <almira.efremenko@yandex.ru>
2022-05-11 23:55:20 +0500

    Add screenStageHardening.

--- Hardening/ScreenStageHardening/ScreenStageHardeningCoordinator.swift
+++ Hardening/ScreenStageHardening/ScreenStageHardeningCoordinator.swift
@@ -5,4 +5,19 @@
 //  Created by MAC on 11.05.2022.
 //
 
-import Foundation
+import UIKit
+
+class ScreenStageHardeningCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let openScreenStageHardeningController = ScreenStageHardeningController()
+        openScreenStageHardeningController.coordinator = self
+        navigationController.pushViewController(openScreenStageHardeningController, animated: false)
+    }
+}
