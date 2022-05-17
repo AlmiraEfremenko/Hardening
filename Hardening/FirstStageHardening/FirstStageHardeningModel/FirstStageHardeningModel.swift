d3f6329bb02a3451781b5cb04de6408bc3ac1e7f
AlmiraEfremenko <almira.efremenko@yandex.ru>
2022-05-17 13:02:09 +0500

    Add firstStageHardeningModel.

--- Hardening/FirstStageHardening/FirstStageHardeningModel/FirstStageHardeningModel.swift
+++ Hardening/FirstStageHardening/FirstStageHardeningModel/FirstStageHardeningModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct FirstStageHardeningModel {
+    var title: String
+    var walkBarefoot: String
+    var sleepOpenWindow: String
+    var washingColdWater: String
+}
+
+extension FirstStageHardeningModel {
+    
+    static var data = FirstStageHardeningModel(title: "Этап 1",
+                                               walkBarefoot: "Хождение босиком дома",
+                                               sleepOpenWindow: "Сон с открытым окном",
+                                               washingColdWater: "Умывание холодной водой утром и вечером")
+}
