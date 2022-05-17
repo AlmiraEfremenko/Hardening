9b158d6154aace8a7ba6fa0af569c7d9e2098f41
AlmiraEfremenko <almira.efremenko@yandex.ru>
2022-05-17 13:02:29 +0500

    Add firstStageHardeningCoordinator.

--- Hardening/FirstStageHardening/FirstStageHardeningCoordinator/FirstStageHardeningCoordinator.swift
+++ Hardening/FirstStageHardening/FirstStageHardeningCoordinator/FirstStageHardeningCoordinator.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import UIKit
+
+class FirstStageHardeningCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let firstStageHardeningCoordinator = FirstStageHardeningController()
+        firstStageHardeningCoordinator.coordinator = self
+        navigationController.pushViewController(firstStageHardeningCoordinator, animated: false)
+    }
+}
