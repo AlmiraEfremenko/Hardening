3a41818b2bb8de6ba06c876b73632c3b2917d6b0
AlmiraEfremenko <almira.efremenko@yandex.ru>
2022-05-17 13:02:48 +0500

    Add firstStageHardeningController.

--- Hardening/FirstStageHardening/FirstStageHardeningController/FirstStageHardeningController.swift
+++ Hardening/FirstStageHardening/FirstStageHardeningController/FirstStageHardeningController.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import UIKit
+
+class FirstStageHardeningController: UIViewController {
+    
+    private var firstStageHardeningView: FirstStageHardeningView? {
+        guard isViewLoaded else { return nil }
+        return view as? FirstStageHardeningView
+    }
+
+   var coordinator: FirstStageHardeningCoordinator?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        self.view = FirstStageHardeningView()
+        
+    }
+}
