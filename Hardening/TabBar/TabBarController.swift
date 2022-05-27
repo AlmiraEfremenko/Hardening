1eb81f6852a3d4aedaf87fb8d90e20c168368069
AlmiraEfremenko <almira.efremenko@yandex.ru>
2022-05-27 19:02:32 +0500

    Add changes tabBar.

--- Hardening/TabBar/TabBarController.swift
+++ Hardening/TabBar/TabBarController.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    var coordinator: TabBarCoordinator?
+
+    override func viewDidLoad() {
+        self.tabBar.tintColor = UIColor(red: 246.0/255.0, green: 88.0/150.0, blue: 40.0/255.0, alpha: 1.0)
+    }
+}
--- Hardening/TabBar/TabBarCoordinator.swift
+++ Hardening/TabBar/TabBarCoordinator.swift
@@ -5,4 +5,38 @@
 //  Created by MAC on 19.05.2022.
 //
 
-import Foundation
+import UIKit
+
+class TabBarCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        
+        let tabBarController = TabBarController()
+        tabBarController.coordinator = self
+        let homeNavigationController = UINavigationController()
+        homeNavigationController.tabBarItem = UITabBarItem(title: "Главная", image: UIImage(systemName: "house.fill"), tag: 0)
+        let homeCoordinator = FirstStageHardeningCoordinator(navigationController: homeNavigationController)
+        
+        let awardsNavigationController = UINavigationController()
+        awardsNavigationController.tabBarItem = UITabBarItem(title: "Награды", image: UIImage(systemName: "snow"), tag: 1)
+        let awardsCoordinator = AwardsCoordinator(navigationController: awardsNavigationController)
+
+        let profileNavigationController = UINavigationController()
+        profileNavigationController.tabBarItem = UITabBarItem(title: "Профиль", image: UIImage(systemName: "person.fill"), tag: 2)
+        let profileCoordinator = ProfileCoordinator(navigationController: profileNavigationController)
+        
+        tabBarController.setViewControllers([homeNavigationController, awardsNavigationController, profileNavigationController], animated: false)
+       
+        navigationController.pushViewController(tabBarController, animated: false)
+        
+        homeCoordinator.start()
+        awardsCoordinator.start()
+        profileCoordinator.start()
+    }
+}
