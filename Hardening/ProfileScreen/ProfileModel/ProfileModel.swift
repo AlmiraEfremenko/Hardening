2b24c4ec6da9c61856313fb0247e921014d0bbd4
AlmiraEfremenko <almira.efremenko@yandex.ru>
2022-06-01 21:27:29 +0500

    Add profileModel.

--- Hardening/ProfileScreen/ProfileModel/ProfileModel.swift
+++ Hardening/ProfileScreen/ProfileModel/ProfileModel.swift
@@ -7,15 +7,10 @@
 
 import Foundation
 
-struct ProfileModel {
-    var name: String
-    var city: String
-    var age: String
-}
+//struct ProfileModel {
+//    var name: String
+//    var city: String
+//    var age: String
+//}
 
-extension ProfileModel {
-    
-    static var data = ProfileModel(name: "Укажите имя",
-                                   city: "Укажите город",
-                                   age: "Укажите возраст")
-}
+
